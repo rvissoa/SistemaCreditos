52e4c862681e3151f0251b1a241055f19948337c
Rodolfo Visso <consultordev19@quimicasuiza.com>
2023-09-04 12:24:00 -0500

    Agregar archivos de proyecto.

---
+++ SistemaCreditos/Controllers/AccesoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaCreditos.Models;
+using System.Diagnostics;
+
+namespace SistemaCreditos.Controllers
+{
+    [AllowAnonymous]
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
