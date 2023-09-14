cf8f375d092b8f126999466969baa9184584bccb
Rodolfo Visso <consultordev19@quimicasuiza.com>
2023-09-14 00:27:37 -0500

    Avances modulo de clientes

---
+++ SistemaCreditos/Controllers/Clientes/ClientesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaCreditos.Models;
+
+namespace SistemaCreditos.Controllers.Clientes
+{
+    public class ClientesController : Controller
+    {
+        private Model db = new Model();
+        public IActionResult ListaClientes()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ListaClientes(string valor)
+        {
+            var model = (from c in db.Clientes
+                        select new { 
+                            dni=c.Dni,
+                            cliente=c.NombreCliente+" "+c.ApellidosCliente,
+                            direccion=c.Direccion+" "+c.Distrito,
+                            celular=c.Celular
+                        }).ToList() ;
+            return new JsonResult(new { success = true, model });
+        }
+
+        public IActionResult NuevoCliente()
+        {
+            return View();
+        }
+    }
+}
