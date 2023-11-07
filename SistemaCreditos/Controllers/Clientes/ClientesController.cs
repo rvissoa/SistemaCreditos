ef6de93cd73e5ac3cc2e5f963e378da5f88d964d
Rodolfo Visso <consultordev19@quimicasuiza.com>
2023-11-07 01:17:15 -0500

    Finalizado de modulo clientes

--- SistemaCreditos/Controllers/Clientes/ClientesController.cs
+++ SistemaCreditos/Controllers/Clientes/ClientesController.cs
@@ -18,10 +18,15 @@ namespace SistemaCreditos.Controllers.Clientes
         {
             var model = (from c in db.Clientes
                         select new { 
+                            id=c.IdCliente,
                             dni=c.Dni,
                             cliente=c.NombreCliente+" "+c.ApellidosCliente,
                             direccion=c.Direccion+" "+c.Distrito,
-                            celular=c.Celular
+                            celular=c.Celular,
+                            codigoGestor = c.CodigoGestor,
+                            zona=c.Zona,
+                            trabajo=c.TrabajoOcupacion,
+                            ubicacionReferencia=c.UbicacionReferencia
                         }).ToList() ;
             return new JsonResult(new { success = true, model });
         }
@@ -90,12 +95,108 @@ namespace SistemaCreditos.Controllers.Clientes
             db.SaveChanges();
             return Json(new { success = true });
         }
+        [HttpPost]
+        public ActionResult Edicion(int id)
+        {
+            var model = (from c in db.Clientes.Where(e => e.IdCliente == id)
+                         select new cliente
+                         {
+                             //idCliente = c.IdCliente,
+                             //codigoCliente = c.CodigoCliente,
+                             gestor = c.CodigoGestor,
+                             nombres = c.NombreCliente,
+                             apellidos = c.ApellidosCliente,
+                             direccion = c.Direccion,
+                             ubicacion = c.UbicacionReferencia,
+                             idDistrito = c.Distrito,
+                             zona = c.Zona,
+                             dni = c.Dni,
+                             celular = c.Celular,
+                             trabajo = c.TrabajoOcupacion,
+                             nombreHijos = c.Hijos,
+
+                             tieneConyuge = c.TieneConyuge == 1 ? true : false,
+                             dniConyuge = c.DniConyuge,
+                             nombresConyuge = c.NombreConyuge,
+                             apellidosConyuges = c.ApellidosConyuge,
+                             direccionConyuge = c.DireccionConyuge,
+                             ubicacionConyuge = c.UbicacionReferenciaConyuge,
+                             trabajoConyuge = c.TrabajoConyuge,
+                             celularConyuge = c.TelefonoConyuge,
+
+                             tieneAval = c.TieneAval == 1 ? true : false,
+                             dniAval = c.DniAval,
+                             nombresAval = c.NombreAval,
+                             apellidosAval = c.ApellidosAval,
+                             direccionAval = c.DireccionAval,
+                             ubicacionAval = c.UbicacionReferencia,
+                             trabajoAval = c.TrabajoAval,
+                             celularAval = c.TelefonoAval,
+                             parentezcoAval = c.ParentescoAval,
+                             observaciones = c.Observaaciones
+                         }).FirstOrDefault();
+
+            return Json(new { success = true, clientes = model });
+
+        }
+
+        public IActionResult EditarCliente(int id)
+        {
+            ViewBag.idCliente = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditarCliente([FromBody] cliente cliente)
+        {
+            var modelAnterior = db.Clientes.Find(cliente.idCliente);
+
+            modelAnterior.IdCliente = Convert.ToInt32(cliente.idCliente);
+            modelAnterior.CodigoGestor = Convert.ToString(cliente.gestor);
+            modelAnterior.NombreCliente = cliente.nombres;
+            modelAnterior.ApellidosCliente = cliente.apellidos;
+            modelAnterior.Direccion = cliente.direccion;
+            modelAnterior.UbicacionReferencia = cliente.ubicacion;
+            modelAnterior.Distrito = cliente.idDistrito;
+            modelAnterior.Zona = cliente.zona;
+            modelAnterior.Dni = cliente.dni;
+            modelAnterior.Celular = cliente.celular;
+            modelAnterior.TrabajoOcupacion = cliente.trabajo;
+            modelAnterior.Hijos = cliente.nombreHijos;
+            modelAnterior.TieneConyuge = cliente.tieneConyuge ? 1 : 0;
+            modelAnterior.DniConyuge = cliente.dniConyuge;
+            modelAnterior.NombreConyuge = cliente.nombresConyuge;
+            modelAnterior.ApellidosConyuge = cliente.apellidosConyuges;
+            modelAnterior.DireccionConyuge = cliente.direccionConyuge;
+            modelAnterior.UbicacionReferenciaConyuge = cliente.ubicacionConyuge;
+            modelAnterior.TrabajoConyuge = cliente.trabajoConyuge;
+            modelAnterior.TelefonoConyuge = cliente.celularConyuge;
+            modelAnterior.TieneAval = cliente.tieneAval ? 1 : 0;
+            modelAnterior.DniAval = cliente.dniAval;
+            modelAnterior.NombreAval = cliente.nombresAval;
+            modelAnterior.ApellidosAval = cliente.apellidosAval;
+            modelAnterior.DireccionAval = cliente.direccionAval;
+            modelAnterior.UbicacionReferenciaAval = cliente.ubicacionAval;
+            modelAnterior.TrabajoAval = cliente.trabajoAval;
+            modelAnterior.TelefonoAval = cliente.celularAval;
+            modelAnterior.ParentescoAval = cliente.parentezcoAval;
+            modelAnterior.Observaaciones = cliente.observaciones;
+            modelAnterior.FechaModifica = DateTime.Now;
+
+            var usuario = @User?.Claims.Where(e => e.Type == "preferred_username").Select(e => e.Value).FirstOrDefault();
+            string[] user = usuario.Split("@");
+            modelAnterior.UsuarioModifica = user[0];
+
+            db.SaveChanges();
+            return Json(new { success = true });
+        }
 
         public class cliente
         {
-            public int idDistrito { get; set; }
+            public int? idCliente { get; set; }
+            public int? idDistrito { get; set; }
             public string zona { get; set; }
-            public int gestor { get; set; }
+            public string gestor { get; set; }
             public string nombres { get; set; }
             public string apellidos { get; set; }
             public string dni { get; set; }
